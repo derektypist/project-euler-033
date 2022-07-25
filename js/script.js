50d91afecb91b89a1270df11898233185719f09c
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-07-25 09:23:59 +0000

    Added simplifyFrac function at script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ function showSolution() {
     // Display Solution in the Browser
     document.getElementById("solution").innerHTML = txt;
 }
+
+// Function to Simplify Fraction
+function simplifyFrac(denom,num) {
+    let sDenom = denom, sNum = num;
+    for (let i=2;i<10;i++) {
+        if (sDenom % i === 0 && sNum % i === 0) {
+            sDenom /= i;
+            sNum /= i;
+            i = 2;
+        }
+    }
+    return [sDenom,sNum];
+}
